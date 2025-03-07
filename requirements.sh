84eda48d8cc0c71d6fddc7efd0e9acea6b7785ab
DJCodesStuff <dhruvjoshi892000@gmail.com>
2025-03-07 18:43:13 -0500

    added shell script for installation

---
+++ requirements.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# List of packages to install
+packages=(
+    "numpy==1.24.4"
+    "pandas"
+    "torch"
+    "wandb"
+    "transformers"
+    "peft"
+    "datasets"
+    "scikit-learn"
+    "ollama"
+    "textacy"
+    "kagglehub"
+    "cython"
+    "murmurhash"
+    "cymem"
+    "preshed"
+    "thinc"
+    "blis"
+    "spacy"
+    "sklearn"
+)
+
+# Loop through each package and install it
+for package in "${packages[@]}"; do
+    echo "Installing $package..."
+    python3 -m pip install "$package"
+done
+
+echo "All packages installed!"
